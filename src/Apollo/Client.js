d4c60d785e23241d8817c51d783df409e4666010
insungsong <song22861@naver.com>
2020-05-24 19:41:01 +0900

    Real Url

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,10 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "http://localhost:4000",
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000"
+      : "https://prismagram-backend-part.herokuapp.com/",
   clientState: {
     //cache값을 받을때 효휼적으로 사용가능하다.
     defaults,
